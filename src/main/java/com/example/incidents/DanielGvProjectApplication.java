ea3bb121e370b78f322e46f69238ebd8f1e5d092
Daniel Garcia Vazquez <garciavd030@gmail.com>
2026-02-20 09:49:18 +0100

    Add files via upload

---
+++ src/main/java/com/example/incidents/DanielGvProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.incidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielGvProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielGvProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/incidents/DanielGvProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.incidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanielGvProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
